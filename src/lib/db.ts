499074217fff60a3366f005d66f91e742ce088df
Mahesh K <maheshkblog@gmail.com>
2025-06-24 20:12:08 +0000

    motherfucker now you deleted the and asking fro new key

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI="mongodb+srv://mahesh:maheshkblog@cluster0.baoom2y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error(
